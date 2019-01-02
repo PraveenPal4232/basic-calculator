86f507234772cad77870c0fddfc7ae0dd3f2c49d
Praveen Pal <praveenpal4232@gmail.com>
2019-01-02 13:24:09 +0530

    add js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function Addfunction(a,b,c){
+		a = Number(value1.value);
+		b = Number(value2.value);
+		c = a + b;
+		main.innerHTML += a + " + " + b + " = " + c + "<br>";
+	};
+	
+	function Substractfunction(a,b,c){
+		a = Number(value1.value);
+		b = Number(value2.value);
+		c = a - b;
+		main.innerHTML += a + " - " + b + " = " + c + "<br>";
+	};
+	
+	function Multiplyfunction(a,b,c){
+		a = Number(value1.value);
+		b = Number(value2.value);
+		c = a * b;
+		main.innerHTML += a + " * " + b + " = " + c + "<br>";
+	};
+	
+	function Dividefunction(a,b,c){
+		a = Number(value1.value);
+		b = Number(value2.value);
+		c = a / b;
+		main.innerHTML += a + " / " + b + " = " + c + "<br>";
+	}
